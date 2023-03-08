662a62f2282f8c1b1b428e2a98e78c24e798060c
Jan William Haug <71844869+JanWilliamHaug@users.noreply.github.com>
2023-03-08 14:03:22 -0800

    Edited the buttons

--- Gui.py
+++ Gui.py
@@ -8,6 +8,7 @@ from docx.shared import Inches
 import tkinter as tk
 from tkinter import *
 from tkinter import filedialog
+from tkinter import ttk
 from typing import Tuple
 
 from tkinter import scrolledtext
@@ -28,52 +29,74 @@ import matplotlib.pyplot as plt
 import Targest2
 
 def GUI1():
+   
     try:
-        # pdb.set_trace()
+       
+            # pdb.set_trace()
         # Creates a word document, saves it as "report 3, and also adds a heading
         
         # Creates the gui
-        window = Tk(className=' TARGEST v.1.5.x ')
+        window = Tk(className=' TARGEST v.1.6.x ')
         # set window size #
-        window.geometry("500x500")
+        window.geometry("600x600")
         window['background'] = '#afeae6'
 
+         # Create a style for the widgets
+        style = ttk.Style()
+        #style.configure('Emergency.TButton', font='helvetica 24', foreground='red', padding=10)
+        style.configure("TButton", font=("Segoe UI", 10), background="#4CAF50", foreground="green")
+
+
+        # Create a canvas on the left side of the window
+        #canvas = tk.Canvas(window, width=200, height=400)
+        #canvas.pack(side='left')
+
+
+        # Load your anime figures as image files
+        #figure1 = tk.PhotoImage(file='eren.png')
+        #figure2 = tk.PhotoImage(file='eren.png')
+
+        # Place your anime figures on the canvas
+        #canvas.create_image(50, 50, image=figure1)
+
         # Creates button 1
-        Button(window, text="Choose list of Documents", command=Targest2.generateReport, width = 26).pack()
+        ttk.Button(window, text="Choose list of Documents", command=Targest2.generateReport, width = 26).pack()
         # Creates button 2
         global genRep
-        genRep = Button(window, text="Generate Reports ", state= DISABLED, command=Targest2.generateReport2, width = 26)
+        genRep = ttk.Button(window, text="Generate Reports ", state= DISABLED, command=Targest2.generateReport2, width = 26)
         genRep.pack()
         global allTagsButton
-        allTagsButton = Button(text="Open all tags Report", state= DISABLED, command=Targest2.getDocumentTable, width = 26)
+        allTagsButton = ttk.Button(text="Open all tags Report", state= DISABLED, command=Targest2.getDocumentTable, width = 26)
         allTagsButton.pack()
         # Creates button 3
         global getDoc
-        getDoc = Button(window, text="Open Generated Report", state= DISABLED, command=Targest2.getDocument, width = 26)
+        getDoc = ttk.Button(window, text="Open Generated Report", state= DISABLED, command=Targest2.getDocument, width = 26)
         getDoc.pack()
         # Creates Excel button button 4
         global getExcel
-        getExcel = Button(text="Open Generated Excel Report", state= DISABLED, command=Targest2.createExcel, width = 26)
+        getExcel = ttk.Button(text="Open Generated Excel Report", state= DISABLED, command=Targest2.createExcel, width = 26)
         getExcel.pack()
         # Creates button 5
         global getOrphan
-        getOrphan = Button(text="Generate Orphan Report", state= DISABLED, command=Targest2.orphanGenReport, width = 26)
+        getOrphan = ttk.Button(text="Generate Orphan Report", state= DISABLED, command=Targest2.orphanGenReport, width = 26)
         getOrphan.pack()
         # Creates button 6
         global getOrphanDoc
-        getOrphanDoc = Button(text="Open Orphan Tags Report", state= DISABLED, command=Targest2.getOrphanDocument, width = 26)
+        getOrphanDoc = ttk.Button(text="Open Orphan Tags Report", state= DISABLED, command=Targest2.getOrphanDocument, width = 26)
         getOrphanDoc.pack()
         
         # Creates button 7
         global button
-        button = Button(text="End Program", command=window.destroy, width = 26)
+        button = Button(text="End Program", command=window.destroy, width = 26, font=("Segoe UI", 10), background="#4CAF50", foreground="white")
         button.pack()
+
 
         # Create text widget and specify size.
         global Txt
         Txt = Text(window, height = 25, width = 55)
         Txt.pack()
 
+        
         msg3 = ('You need a text file with paths to your documents\n 1. Please choose your documents by clicking on \n    the "choose document" button.\n 2. Click "Generate Reports".  \n\n')
         Txt.insert(tk.END, msg3) #print in GUI
         

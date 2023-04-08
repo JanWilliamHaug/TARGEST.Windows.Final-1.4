542c47771cf7868dd00db7202f3dcb21d34e9424
JanWilliamHaug <71844869+JanWilliamHaug@users.noreply.github.com>
2023-04-08 15:37:53 -0700

    Newest version of gui

--- Gui.py
+++ Gui.py
@@ -39,7 +39,7 @@ def GUI1():
    
     try:
         # Creates the gui
-        window = Tk(className=' TARGEST v.1.15.1 ')
+        window = Tk(className=' TARGEST v.1.16.1 ')
         # set window size #
         window.geometry("1000x750")
         window['background'] = '#afeae6'

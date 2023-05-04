3b14c8a81e10fde134a3d201e12dc86a6b397253
JanWilliamHaug <71844869+JanWilliamHaug@users.noreply.github.com>
2023-05-04 13:13:50 -0700

    modifed TBV and TBD methods

--- Targest2.py
+++ Targest2.py
@@ -746,6 +746,7 @@ def generateReport2():
                                                         
                                                         if "TBD:" in item:
                                                             TBDReport.add_paragraph(item)
+                                                            TBDTags.append(item)
                                                             if str(item) in dicts2Copy:
                                                                 para2 = TBDReport.add_paragraph(dicts2Copy[str(item)])
                                                                 para2.paragraph_format.left_indent = Inches(0.25) # adds indentation of text
@@ -753,6 +754,7 @@ def generateReport2():
 
                                                         if "TBV:" in item:
                                                             TBVReport.add_paragraph(item)
+                                                            TBVTags.append(item)
                                                             if str(item) in dicts2Copy:
                                                                 para2 = TBVReport.add_paragraph(dicts2Copy[str(item)])
                                                                 para2.paragraph_format.left_indent = Inches(0.25) # adds indentation of text
@@ -916,6 +918,7 @@ def generateReport2():
                                                     
                                                     if "TBD:" in item:
                                                             TBDReport.add_paragraph(item)
+                                                            TBDTags.append(item)
                                                             if str(item) in dicts2Copy:
                                                                 para2 = TBDReport.add_paragraph(dicts2Copy[str(item)])
                                                                 para2.paragraph_format.left_indent = Inches(0.25) # adds indentation of text
@@ -923,6 +926,7 @@ def generateReport2():
 
                                                     if "TBV:" in item:
                                                             TBVReport.add_paragraph(item)
+                                                            TBVTags.append(item)
                                                             if str(item) in dicts2Copy:
                                                                 para2 = TBVReport.add_paragraph(dicts2Copy[str(item)])
                                                                 para2.paragraph_format.left_indent = Inches(0.25) # adds indentation of text
